ecdda42ac6a4ab7228e0361070a1ec9e85da3c9b
FarizIbrahimov19866 <fibrahimov19866@ada.edu.az>
2026-02-26 15:10:15 +0400

    Add EventServiceImpl:{getEventsByPriceRange} implementation

--- src/main/java/az/edu/ada/wm2/lab5/service/EventServiceImpl.java
+++ src/main/java/az/edu/ada/wm2/lab5/service/EventServiceImpl.java
@@ -113,17 +113,24 @@ public class EventServiceImpl implements EventService {
 
     @Override
     public List<Event> getEventsByPriceRange(BigDecimal minPrice, BigDecimal maxPrice) {
-        return List.of();
-    }
-    @Override
-    public List<Event> getEventsByPriceRange(BigDecimal minPrice, BigDecimal maxPrice) {
-       return List.of();
+
+        if (minPrice == null || maxPrice == null || minPrice.compareTo(maxPrice) > 0) {
+            return List.of();
+        }
+
+        return eventRepository.findAll()
+                .stream()
+                .filter(event -> event.getTicketPrice() != null &&
+                        event.getTicketPrice().compareTo(minPrice) >= 0 &&
+                        event.getTicketPrice().compareTo(maxPrice) <= 0)
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Event> getEventsByDateRange(LocalDateTime start, LocalDateTime end) {
         return List.of();
     }
+
 
     @Override
     public Event updateEventPrice(UUID id, BigDecimal newPrice) {
